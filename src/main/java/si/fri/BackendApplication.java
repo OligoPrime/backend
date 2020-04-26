a1268c9504eb88ac31dc84f019071f06c33abca4
UrbanKocmut <u.kocmut@gmail.com>
2020-04-26 10:07:58 +0200

    enable CORS

--- src/main/java/si/fri/BackendApplication.java
+++ src/main/java/si/fri/BackendApplication.java
@@ -16,6 +16,7 @@ import io.dropwizard.setup.Environment;
 import org.glassfish.jersey.media.multipart.MultiPartFeature;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import org.eclipse.jetty.servlets.CrossOriginFilter;
 import org.glassfish.jersey.server.filter.RolesAllowedDynamicFeature;
 import si.fri.auth.SimpleAuthenticator;
 import si.fri.auth.SimpleAuthorizer;
@@ -30,7 +31,10 @@ import si.fri.resources.PrimerResource;
 import si.fri.resources.UserResource;
 import si.fri.resources.CsvResource;
 
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
 import java.security.Key;
+import java.util.EnumSet;
 
 public class BackendApplication extends Application<BackendConfiguration> {
 
@@ -60,9 +64,27 @@ public class BackendApplication extends Application<BackendConfiguration> {
         bootstrap.setConfigurationSourceProvider(new SubstitutingSourceProvider(bootstrap.getConfigurationSourceProvider(), new EnvironmentVariableSubstitutor()));
     }
 
+
+    private void configureCors(Environment environment) {
+        final FilterRegistration.Dynamic cors =
+                environment.servlets().addFilter("CORS", CrossOriginFilter.class);
+
+        // Configure CORS parameters
+        cors.setInitParameter(CrossOriginFilter.ALLOWED_ORIGINS_PARAM, "*");
+        cors.setInitParameter(CrossOriginFilter.ALLOWED_HEADERS_PARAM, "X-Requested-With,Content-Type,Accept,Origin,Authorization");
+        cors.setInitParameter(CrossOriginFilter.ALLOWED_METHODS_PARAM, "OPTIONS,GET,PUT,POST,DELETE,HEAD");
+        cors.setInitParameter(CrossOriginFilter.ALLOW_CREDENTIALS_PARAM, "true");
+
+        // Add URL mapping
+        cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
+
+    }
+
     @Override
     public void run(final BackendConfiguration configuration,
                     final Environment environment) {
+        configureCors(environment);
+
         final HelloDAO helloDAO = new HelloDAO(hibernate.getSessionFactory());
         final UserDAO userDAO = new UserDAO(hibernate.getSessionFactory());
         final PrimerDAO primerDAO = new PrimerDAO(hibernate.getSessionFactory());

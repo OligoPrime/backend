892e6345d82c5e505c83277c5f65dc581b92830f
auxermen <auxermen@gmail.com>
2020-05-09 21:17:49 +0200

    implement inserting primer into db from csv file

--- src/main/java/si/fri/resources/CsvResource.java
+++ src/main/java/si/fri/resources/CsvResource.java
@@ -1,30 +1,41 @@
 package si.fri.resources;
 
-import com.opencsv.CSVParser;
-import com.opencsv.CSVParserBuilder;
-import com.opencsv.CSVReader;
-import com.opencsv.CSVReaderBuilder;
+import com.opencsv.bean.CsvBindByName;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+import io.dropwizard.hibernate.UnitOfWork;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.io.IOUtils;
 import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
 import org.glassfish.jersey.media.multipart.FormDataParam;
+import si.fri.core.Primer;
+import si.fri.core.primer_enums.*;
+import si.fri.db.PrimerDAO;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.io.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 @Path("/csv")
 @Produces(MediaType.APPLICATION_JSON)
 public class CsvResource {
 
-    public CsvResource() {
+    private final PrimerDAO dao;
 
+    public CsvResource(PrimerDAO dao) {
+        this.dao = dao;
     }
 
     @POST
     @Path("/import")
     @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @UnitOfWork
     public Response uploadFile(
             @FormDataParam("file") InputStream uploadedInputStream,
             @FormDataParam("file") FormDataContentDisposition fileDetail) throws IOException {
@@ -36,20 +47,68 @@ public class CsvResource {
             return Response.status(Response.Status.UNSUPPORTED_MEDIA_TYPE).build();
         }
 
-        CSVParser csvParser = new CSVParserBuilder().withSeparator(';').build();
-        CSVReader csvReader = new CSVReaderBuilder(new InputStreamReader(uploadedInputStream)).withCSVParser(csvParser).build();
+        CsvToBean<PrimerCSV> csvToBean = new CsvToBeanBuilder(new InputStreamReader(uploadedInputStream))
+                .withType(PrimerCSV.class)
+                .withIgnoreLeadingWhiteSpace(true)
+                .withSeparator(';')
+                .build();
 
-        String[] nextRecord;
-        
-        // prints csv in console
-        while ((nextRecord = csvReader.readNext()) != null) {
-            for (String cell : nextRecord) {
-                System.out.print(cell + "\t");
+        List<PrimerCSV> PrimerCSVList = new ArrayList<>();
+
+        for (PrimerCSV primerCsv : csvToBean) {
+            if (primerCsv.typeOfPrimer.equals("TaqProbe")) {
+                if (primerCsv.sequence == null) {
+                    throw new IllegalArgumentException("Attribute 'sequence' must not be empty!");
+                }
+                else if (primerCsv.sequence.isEmpty()) {
+                    throw new IllegalArgumentException("Attribute 'sequence' must not be empty!");
+                }
+                if (primerCsv.assayId == null) {
+                    throw new IllegalArgumentException("Attribute 'assayId' must not be empty!");
+                }
+                else if (primerCsv.assayId.isEmpty()) {
+                    throw new IllegalArgumentException("Attribute 'assayId' must not be empty!");
+                }
+                if (primerCsv.size == null) {
+                    throw new IllegalArgumentException("Attribute 'size' must not be empty!");
+                }
             }
-            System.out.println();
+            SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy");
+            try {
+                format.parse(primerCsv.date);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+
+            PrimerCSVList.add(primerCsv);
         }
 
-        return Response.ok("CSV file successfully uploaded.").build();
+        for (PrimerCSV p : PrimerCSVList) {
+            SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy");
+            Date date = null;
+            try {
+                date = format.parse(p.date);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+
+            Primer primer = new Primer(p.name, p.sequence, Orientation.fromString(p.orientation), dao.findFreezer(p.freezer),
+                    dao.findDrawer(p.drawer), dao.findBox(p.box), dao.findPositionInReference(p.positionInReference), p.Tm,
+                    p.optimalTOfAnnealing, dao.findPurificationMethod(p.purificationMethod), p.amountAvailableMikroL,
+                    p.amountAvailablePacks, AmountAvailablePackType.fromString(p.amountAvailablePackType), p.lengthOfAmplicone,
+                    p.storingT, p.GCPercent, dao.findOrganism(p.organism), dao.findGen(p.gen), dao.findNcbiGenId(p.ncbiGenId),
+                    dao.findHumanGenomBuild(p.humanGenomBuild), dao.findFormulation(p.formulation), dao.findTypeOfPrimer(p.typeOfPrimer),
+                    p.sondaSequence, p.assayId, Size.fromString(p.size), dao.findPrimerApplication(p.primerApplication),
+                    p.applicationComment, dao.findFiveModification(p.fiveModification), dao.findThreeModification(p.threeModification),
+                    p.concentrationOrdered, ConcentrationOrderedUnit.fromString(p.concentrationOrderedUnit), p.checkSpecifityInBlast,
+                    dao.findDesignerName(p.designerName), dao.findDesignerPublication(p.designerPublication),
+                    dao.findDesignerDatabase(p.designerDatabase), dao.findProject(p.project), dao.findSupplier(p.supplier),
+                    dao.findManufacturer(p.manufacturer), p.comment, p.document, p.analysis, OrderStatus.fromString(p.orderStatus),
+                    dao.findThreeQuencher(p.threeQuencher), dao.findFiveDye(p.fiveDye), date, null);
+            dao.create(primer);
+        }
+
+        return Response.ok("Successfully uploaded primers from CSV file.").build();
     }
 
     // save uploaded file to new location
@@ -78,4 +137,97 @@ public class CsvResource {
     //@GET("export")
     //@UnitOfWork
     // TODO
+
+    public static class PrimerCSV {
+        @CsvBindByName(column = "Name of Primer", required = true)
+        public String name;
+        @CsvBindByName(column = "Sequence")
+        public String sequence;
+        @CsvBindByName(column = "Orientation")
+        public String orientation;
+        @CsvBindByName(column = "Freezer", required = true)
+        public String freezer;
+        @CsvBindByName(column = "Drawer", required = true)
+        public String drawer;
+        @CsvBindByName(column = "Box", required = true)
+        public String box;
+        @CsvBindByName(column = "Position in Reference", required = true)
+        public String positionInReference;
+        @CsvBindByName(column = "Tm (°C)")
+        public Double Tm;
+        @CsvBindByName(column = "Optimal T of Annealing (°C)")
+        public Double optimalTOfAnnealing;
+        @CsvBindByName(column = "Purification Method", required = true)
+        public String purificationMethod;
+        @CsvBindByName(column = "Amount Available (µl)")
+        public Double amountAvailableMikroL;
+        @CsvBindByName(column = "Amount Available (Packs)")
+        public Integer amountAvailablePacks;
+        @CsvBindByName(column = "Amount Available (Pack Type)")
+        public String amountAvailablePackType;
+        @CsvBindByName(column = "Date")
+        public String date;
+        @CsvBindByName(column = "Length of Amplicone")
+        public Integer lengthOfAmplicone;
+        @CsvBindByName(column = "Storing T (°C)")
+        public String storingT;
+        @CsvBindByName(column = "GC (%)")
+        public Double GCPercent;
+        @CsvBindByName(column = "Organism", required = true)
+        public String organism;
+        @CsvBindByName(column = "Gen", required = true)
+        public String gen;
+        @CsvBindByName(column = "Ncbi Gen ID")
+        public String ncbiGenId;
+        @CsvBindByName(column = "Human Genom Build")
+        public String humanGenomBuild;
+        @CsvBindByName(column = "Formulation", required = true)
+        public String formulation;
+        @CsvBindByName(column = "Type of Primer", required = true)
+        public String typeOfPrimer;
+        @CsvBindByName(column = "Sonda Sequence")
+        public String sondaSequence;
+        @CsvBindByName(column = "Assay ID")
+        public String assayId;
+        @CsvBindByName(column = "Size")
+        public String size;
+        @CsvBindByName(column = "Application", required = true)
+        public String primerApplication;
+        @CsvBindByName(column = "Application Comment")
+        public String applicationComment;
+        @CsvBindByName(column = "5' Modification", required = true)
+        public String fiveModification;
+        @CsvBindByName(column = "3' Modification", required = true)
+        public String threeModification;
+        @CsvBindByName(column = "Concentration Ordered")
+        public Double concentrationOrdered;
+        @CsvBindByName(column = "Concentration Ordered Unit")
+        public String concentrationOrderedUnit;
+        @CsvBindByName(column = "Did You Check Specifity in Blast?")
+        public Boolean checkSpecifityInBlast;
+        @CsvBindByName(column = "Designer Name")
+        public String designerName;
+        @CsvBindByName(column = "Designer Publication")
+        public String designerPublication;
+        @CsvBindByName(column = "Designer Database")
+        public String designerDatabase;
+        @CsvBindByName(column = "Project")
+        public String project;
+        @CsvBindByName(column = "Supplier")
+        public String supplier;
+        @CsvBindByName(column = "Manufacturer")
+        public String manufacturer;
+        @CsvBindByName(column = "Comment")
+        public String comment;
+        @CsvBindByName(column = "Document")
+        public String document;
+        @CsvBindByName(column = "Analysis")
+        public String analysis;
+        @CsvBindByName(column = "Order Status")
+        public String orderStatus;
+        @CsvBindByName(column = "3' Quencher")
+        public String threeQuencher;
+        @CsvBindByName(column = "5' Dye")
+        public String fiveDye;
+    }
 }
